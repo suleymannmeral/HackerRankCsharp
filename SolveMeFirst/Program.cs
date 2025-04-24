76038593837a13fc6b5df1c97338bb65ca131871
suleymannmeral <mrlslymn02@gmail.com>
2025-04-24 20:47:59 +0300

    SolveMeFirst

---
+++ SolveMeFirst/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using hackerRank1;
+
+
+int val1 = Convert.ToInt32(Console.ReadLine());
+int val2 = Convert.ToInt32(Console.ReadLine());
+int sum = Solution.SolveMefirst(val1, val2);
+Console.WriteLine(sum);
